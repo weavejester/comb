4199a470195150e47be6515ee5e26935f9e116c4
James Reeves <jreeves@weavejester.com>
2011-03-07 22:53:19 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject comb "0.1.0"
+  :description "Clojure templating library"
+  :dependencies [[org.clojure/clojure "1.2.0"]])
---
+++ src/comb/template.clj
@@ -0,0 +1,35 @@
+(ns comb.template
+  "Clojure templating library."
+  (:refer-clojure :exclude [compile eval]))
+
+(defn- read-source [source]
+  (if (string? source)
+    source
+    (slurp source)))
+
+(def delimiters ["<%" "%>"])
+
+(def parser-regex
+  (re-pattern
+   (str "(?s)\\A"
+        "(?:" "(.*?)"
+        (first delimiters) "(.*?)" (last delimiters)
+        ")?"
+        "(.*)\\z")))
+
+(defn- parse-string [src]
+  (lazy-seq
+   (let [[_ before expr after] (re-matches parser-regex src)]
+     (if expr
+       (list* before
+              (read-string expr) 
+              (parse-string after))
+       (list after)))))
+
+(defmacro compile
+  "Compile a template into a function that takes the supplied arguments. The
+  template source of the template may be a string, or an IO source such as a
+  File, Reader or InputStream."
+  [source args]
+  `(fn ~args
+     (str ~@(parse-string (read-source source)))))
