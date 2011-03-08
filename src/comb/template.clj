2459632d73a70cd4c650c2184286889c86ae63bd
James Reeves <jreeves@weavejester.com>
2011-03-08 00:13:49 +0000

    Template compiled at run-time

--- src/comb/template.clj
+++ src/comb/template.clj
@@ -1,6 +1,7 @@
 (ns comb.template
   "Clojure templating library."
-  (:refer-clojure :exclude [fn eval]))
+  (:refer-clojure :exclude [fn eval])
+  (:require [clojure.core :as core]))
 
 (defn- read-source [source]
   (if (string? source)
@@ -26,10 +27,14 @@
               (parse-string after))
        (list after)))))
 
+(defn compile-fn [args src]
+  (core/eval
+   `(core/fn ~args
+      (str ~@(parse-string (read-source src))))))
+
 (defmacro fn
   "Compile a template into a function that takes the supplied arguments. The
   template source of the template may be a string, or an IO source such as a
   File, Reader or InputStream."
   [args source]
-  `(clojure.core/fn ~args
-     (str ~@(parse-string (read-source source)))))
+  `(compile-fn '~args ~source))
