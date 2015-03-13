b75d985647d3105026d9d95d0708c876629aa70f
James Reeves <jreeves@weavejester.com>
2015-03-13 16:23:38 +0000

    Add better project description and URL

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject comb "0.1.0"
-  :description "Clojure templating library"
+  :description "Clojure templating library similar to ERB"
+  :url "https://github.com/weavejester/comb"
   :dependencies [[org.clojure/clojure "1.2.1"]])
